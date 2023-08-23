7e33b937789676d6bffa45d61b09943de1cf0096
Iury de Amorim Demberg <iuryamorimdev@gmail.com>
2023-08-23 19:37:08 -0400

    praticando js

---
+++ aula11/ex001.js
@@ -0,0 +1,7 @@
+
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Voce ultrapassou a velocidade permitida, MUITADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
