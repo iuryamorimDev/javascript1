78891feac2929a5fa98700817ff2f5a19e8dcda1
Iury de Amorim Demberg <iuryamorimdev@gmail.com>
2023-09-11 15:49:04 -0400

    ex concluido

--- ex16/ex001.js
+++ ex16/ex001.js
@@ -1,1 +1,33 @@
-alert('Olá')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO]faltam dados!')
+    } else {
+        res.innerHTML = 'contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('passo invalido! considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for(let c =1; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+            
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+        
+        
+    }
+}
