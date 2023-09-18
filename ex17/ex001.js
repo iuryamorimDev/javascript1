3641096a992527a5a096e716e42e84c6d4326c21
Iury de Amorim Demberg <iuryamorimdev@gmail.com>
2023-09-18 18:20:07 -0400

    criando ex

--- ex17/ex001.js
+++ ex17/ex001.js
@@ -1,1 +1,29 @@
-alert('Olá')
+let num = document.querySelector("input#fnum")
+let lista = document.querySelector("select#flista")
+let res = document.querySelector("div#res")
+let valores = []
+
+function isNumero(n) {
+  if(Number(n) >= 1 && Number(n) <= 100) {
+    return true
+  }  else {
+    return false
+  }        
+}
+
+
+function inLista(n, l) {
+  if (l.indexOf(Number(n)) != -1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function adcionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)){  
+          
+    } else {
+        window.alert('Valor invalido ou ja encontrado na lista.')
+    }
+}
