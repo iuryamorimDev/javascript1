77268dfb444a3e4308089f863e44a9136c77a7ef
Iury de Amorim Demberg <iuryamorimdev@gmail.com>
2024-01-26 19:39:15 -0500

    ...

---
+++ ex19/script.js
@@ -0,0 +1,15 @@
+const imagens = document.querySelectorAll("#galeria li img");
+
+function galeriaTrocar(event) {
+  const principal = document.querySelector("#imagem-principal");
+  const clicada = event.currentTarget;
+  principal.src = clicada.src;
+  principal.alt = clicada.alt;
+  console.log(clicada.src);
+}
+
+function galeriaClique(imagem) {
+  imagem.addEventListener("click", galeriaTrocar);
+}
+
+imagens.forEach(galeriaClique);
